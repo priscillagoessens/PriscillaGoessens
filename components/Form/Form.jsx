3e14c96484b65c64ac71ac754f1ae7556a6b297b
priscilla goessens <priscillagoessens@gmail.com>
2024-05-22 17:32:48 +0200

    netlify form again

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -14,7 +14,8 @@ export default function Form() {
           setError(null);
           const myForm = event.target;
           const formData = new FormData(myForm);
-          const res = await fetch(`/__forms.html`, {
+
+          const res = await fetch('/__forms.html', {
               method: 'POST',
               headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
               body: new URLSearchParams(formData).toString()
