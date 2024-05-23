39fd0ba871fbbf284c64f627e1faada86ca641f1
priscilla goessens <priscillagoessens@gmail.com>
2024-05-23 11:19:37 +0200

    moving head next

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
 import "./globals.css";
-import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <Head>
-        <title>
-          Priscilla Goessens, Developpeuse front end
-        </title>
-        <meta name="description" content="Priscilla Goessens Portfolio" key="desc" />
-      </Head>
       <body >{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -4,10 +4,18 @@ import Skills from "@/components/Skills/Skills";
 import Form from "@/components/Form/Form";
 import Footer from "@/components/Footer/Footer";
 import Gallery from "@/components/Gallery/Gallery";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          Priscilla Goessens, Developpeuse front end
+        </title>
+        <meta name="description" content="Priscilla Goessens Portfolio" key="desc" />
+      </Head>
+    
       <Header/>
       <About/>
       <Gallery/>
